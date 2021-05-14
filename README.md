655f2d807c3451b701e6d2ffe4304001558f7cb0
Vinay Sharma <>
2021-05-14 19:33:09 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+### WEATHER-APP
+_PWA: Built using React.js and openweatherAPI_
+
+#### Tech Stack
+1. React.js , UI library by facebook
+2. PWA functionality: Chrome web browser
+3. Axios API : Promise based HTTP client for the browser and node.js
+
+### Future Goals
+1. Cancel button
+2. PWA notifications
+3. Offline work mode
---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { FetchWeather } from './api/FetchWeather';
+import { useState } from "react";
+
+import './App.css';
+
+const App = () => {
+
+const [query, setQuery] = useState('');
+const [weather , setWeather] = useState({});
+
+const search = async (e) => {
+  if(e.key === 'Enter') {
+    
+    const data = await FetchWeather(query);
+    setWeather(data);
+    setQuery('');
+  }
+}
+
+    return (
+        <div className="main-container">
+            <input type="text"className="search"placeholder="Search City..."value={query}onChange={(e) => setQuery(e.target.value)}onKeyPress={search}/>
+            {weather.main && (
+                <div className="city">
+                    <h2 className="city-name">
+                        <span>{weather.name}</span>
+                        <sup>{weather.sys.country}</sup>
+                    </h2>
+                    <div className="city-temp">
+                        {Math.round(weather.main.temp)}
+                        <sup>&deg;C</sup>
+                    </div>
+                    <div className="info">
+                        <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+                        <p>{weather.weather[0].description}</p>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default App;
