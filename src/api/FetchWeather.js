0a4df3db1a0bc80c8977cbdf64dcd0148bce6356
Vinay Sharma <vinay02856@gmail.com>
2021-05-27 23:50:21 +0530

    refactor: fetchweather

--- src/api/FetchWeather.js
+++ src/api/FetchWeather.js
@@ -4,14 +4,12 @@ const URL = 'https://api.openweathermap.org/data/2.5/weather';
 const API_KEY = 'f33a484cf794d08d0148764789aaba32';
 
 export const FetchWeather =  async (query) => {
-  const { data } = await axios.get(URL, {
-  
+  const { data } = await axios.get(URL, { 
   params: {
      q: query,
      units:'metric',
      APPID: API_KEY,
-   }
-    
+   }    
   });
   return data;
 };
